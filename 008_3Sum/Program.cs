261e64abd67f8c78944cb0191a710f0fde67700b
siuon <sotom@sotom.tw>
2020-07-09 20:39:21 +0800

    add day 8, 3Sum, not complete yet :(

---
+++ 008_3Sum/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _008_3Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+               Given an array nums of n integers, are there elements a, b, c in nums such that a + b + c = 0? 
+               Find all unique triplets in the array which gives the sum of zero.
+   
+               Note:
+   
+               The solution set must not contain duplicate triplets.
+   
+               Example:
+   
+               Given array nums = [-1, 0, 1, 2, -1, -4],
+   
+               A solution set is:
+               [
+                 [-1, 0, 1],
+                 [-1, -1, 2]
+               ]
+            */
+
+            var nums = new int[] { -1, 0, 1, 2, -1, -4 };
+        }
+    }
+
+    public class Solution
+    {
+        public IList<IList<int>> ThreeSum(int[] nums)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
