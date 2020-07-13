d8580cc2f6f13778d0e428ff963fdc8a5b2038f0
siuon <sotom@sotom.tw>
2020-07-13 23:07:42 +0800

    add day 11, Subsets, not complete yet :(

---
+++ 011_Subsets/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Linq;
+using System;
+using System.Collections.Generic;
+
+namespace _011_Subsets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+               Given a set of distinct integers, nums, return all possible subsets (the power set).
+   
+               Note: The solution set must not contain duplicate subsets.
+   
+               Example:
+               Input: nums = [1,2,3]
+               Output:
+               [
+                 [3],
+                 [1],
+                 [2],
+                 [1,2,3],
+                 [1,3],
+                 [2,3],
+                 [1,2],
+                 []
+               ]
+            */
+
+            var nums = new int[] { 1, 2, 3 };
+
+            var solution = new Solution();
+
+            solution.Subsets(nums);
+        }
+    }
+
+    public class Solution
+    {
+        public IList<IList<int>> Subsets(int[] nums)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
