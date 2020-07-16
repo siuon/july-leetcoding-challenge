1f21962413e1ca51e1e5de09668132c152b7cb56
siuon <sotom@sotom.tw>
2020-07-16 21:57:07 +0800

    add day 15, Reverse Words in a String, not complete yet :(

---
+++ 015_Reverse_Words_in_a_String/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _015_Reverse_Words_in_a_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                Given an input string, reverse the string word by word.
+
+                Example 1:
+                Input: "the sky is blue"
+                Output: "blue is sky the"
+
+                Note:
+                  A word is defined as a sequence of non-space characters.
+                  Input string may contain leading or trailing spaces. However, 
+                    your reversed string should not contain leading or trailing spaces.
+                  You need to reduce multiple spaces between two words to a single space in the reversed string.
+
+                Follow up:
+                For C programmers, try to solve it in-place in O(1) extra space.
+            */
+
+            var s = "the sky is blue";
+
+            var solution = new Solution();
+
+            var expect = "blue is sky the";
+
+            var output = solution.ReverseWords(s);
+
+            Console.WriteLine(s);
+            Console.WriteLine(output);
+
+            Console.WriteLine(output == expect);
+        }
+    }
+
+    public class Solution
+    {
+        public string ReverseWords(string s)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
