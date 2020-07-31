7c9eecc83198566f03de702b2fbbabf206d55836
siuon <sotom@sotom.tw>
2020-07-31 23:55:04 +0800

    add day 30, Word Break II, not complete yet :(

---
+++ 030_Word_Break_II/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _030_Word_Break_II
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                Given a non-empty string s and a dictionary wordDict containing a list of non-empty words, 
+                add spaces in s to construct a sentence where each word is a valid dictionary word. Return all such possible sentences.
+
+                Note:
+                The same word in the dictionary may be reused multiple times in the segmentation.
+                You may assume the dictionary does not contain duplicate words.
+
+                Example 1:
+                Input:
+                s = "catsanddog"
+                wordDict = ["cat", "cats", "and", "sand", "dog"]
+                Output:
+                [
+                    "cats and dog",
+                    "cat sand dog"
+                ]
+            */
+
+            var s = "catsanddog";
+            var wordDict = new List<string>() { "cat", "cats", "and", "sand", "dog" };
+
+            var solution = new Solution();
+
+            solution.WordBreak(s, wordDict);
+        }
+    }
+
+    public class Solution
+    {
+        public IList<string> WordBreak(string s, IList<string> wordDict)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
